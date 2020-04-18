caaf31ce542d9cbf9223e02e68520c74e18e6795
Eslam Ibrahim Shahin <eslamshahin552000@gmail.com>
2020-04-18 06:03:00 +0200

    Init the model

---
+++ Model.py
@@ -0,0 +1,19 @@
+# * Machine Learining Model To Predict Wheather Temperature
+#
+# * Copyright 2020 : Eslam Ibrahim Shahin
+# * FACEBOOK : Eslam Ibrahim Shahin
+# * GMAIL :eslamshahin552000@gmail.com
+#
+# * This model is VERY FLIXABLE MODEL YOU CAN USE
+#   SIMPILE LINEAR REGRESSION OR MULTUABLE LINEAR REGRESSION
+#   BASED ON USER CHOISE
+#
+# * Using Data to train and test the model and
+#   use some libraries like pandas , matplot and scikit
+#
+# * Should running in Python 3
+#
+# *
+#
+#
+#DESCRIPTION
