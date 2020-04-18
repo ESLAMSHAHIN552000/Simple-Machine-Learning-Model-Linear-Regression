1ec42abe6d33f955575193d6f52bf4c0707a729e
Eslam Ibrahim Shahin <eslamshahin552000@gmail.com>
2020-04-18 07:34:17 +0200

    add new feature (enable user to put a specific year to predict weather in it )

--- Model.py
+++ Model.py
@@ -4,9 +4,8 @@
 # * FACEBOOK : Eslam Ibrahim Shahin
 # * GMAIL :eslamshahin552000@gmail.com
 #
-# * This model is VERY FLIXABLE MODEL YOU CAN USE
-#   SIMPILE LINEAR REGRESSION OR MULTUABLE LINEAR REGRESSION
-#   BASED ON USER CHOISE
+# * This model is
+#   SIMPILE LINEAR REGRESSION
 #
 # * Using Data to train and test the model and
 #   use some libraries like pandas , matplot and scikit
@@ -17,3 +16,60 @@
 #
 #
 #DESCRIPTION
+
+#Import libraries
+import pandas as pd
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+
+
+#Read the Dataset 
+dataset = pd.read_csv('Weather0.csv')
+#dataset = pd.read_xlsx('WeatherM.xlsx')# if you have Exel file 
+
+#print(dataset.describe())
+X=["Year"]
+y=["MeanTemp"]
+
+# our data points on a 2-D graph 
+dataset.plot(x='Year', y='MeanTemp', style='o')  
+plt.title('Year vs Mean Temperature')  
+plt.xlabel('Year')  
+plt.ylabel('Mean Temperature')  
+plt.show()
+
+#Here we use 90% from our data set to train our model 
+X_train, X_test, y_train, y_test = train_test_split( dataset[X].values , dataset[y].values, test_size=0.9, random_state=0)
+
+#to train our algorithm. For that, we need to import LinearRegression class,
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+y_pred = regressor.predict(X_test)
+#Checking our predicted data vs real data
+df = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
+print (df.head(10))
+#Ploting predicted data vs real data
+df1 = df.head(10)
+df1.plot(kind='bar',figsize=(12,8))
+plt.grid(which='major', linestyle='-', linewidth='0.8', color='red')
+plt.grid(which='minor', linestyle=':', linewidth='0.8', color='blue')
+plt.show()
+
+#For retrieving the slope:
+print ("the slope")
+print(regressor.coef_)
+
+
+#ploting our test data
+plt.scatter(X_test, y_test,  color='gray')
+plt.plot(X_test, y_pred, color='red', linewidth=2)
+plt.show()
+
+#adding a specific year to predict weather by user
+print ("Input the year you want to predict weather in it : ")
+inp_ur_dataf=list()
+inp_ur_dataf.append(int(input()))
+print(regressor.predict([inp_ur_dataf]),"\n")
+
